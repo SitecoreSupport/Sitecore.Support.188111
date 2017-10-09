3a6045e91bee90d5abb9d64eaa6990673a73d557
DK\rbi <rbi@sitecore.net>
2017-10-09 19:21:48 +0300

    Initial commit. Ony custom processors added.

---
+++ src/Sitecore.Support.188111/ContentTesting/Pipelines/CancelTest/UnprotectTestedItems.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.ContentTesting.Pipelines.CancelTest
+{
+    using Data;
+    using Data.Items;
+    using Diagnostics;
+    using Sitecore.ContentTesting.Pipelines.CancelTest;
+
+    public class UnprotectTestedItems : CancelTestProcessor
+    {
+        public override void Process(CancelTestArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+
+            if (args.TestDefinition != null)
+            {
+                foreach (Item item in args.TestDefinition.InnerItem.Axes.GetDescendants())
+                {
+                    if (item.Fields["Datasource"] != null)
+                    {
+                        Item testedItem = item.Database.GetItem(DataUri.Parse(item.Fields["Datasource"].Value));
+
+                        if (testedItem != null)
+                        {
+                            testedItem.Editing.BeginEdit();
+                            testedItem.Appearance.ReadOnly = false;
+                            testedItem.Editing.EndEdit();
+                        }
+                        else
+                        {
+                            string errorMessage =
+                            string.Format(
+                                "Tested item is null. Please check if the \"Datasource\" field of the test definition item with ID = {0} is not empty.",
+                                item.ID.ToString());
+
+                            Log.Error(errorMessage, this);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/Sitecore.Support.188111/ContentTesting/Pipelines/StartPageTest/ProtectTestedItems.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.ContentTesting.Pipelines.StartPageTest
+{
+    using Data;
+    using Data.Items;
+    using Diagnostics;
+    using Sitecore.ContentTesting.Pipelines.StartPageTest;
+
+    public class ProtectTestedItems : StartPageTestProcessor
+    {
+        public override void Process(StartPageTestArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+
+            if (args.Test != null)
+            {
+                foreach (Item item in args.Test.InnerItem.Axes.GetDescendants())
+                {
+                    if (item.Fields["Datasource"] != null)
+                    {
+                        Item testedItem = item.Database.GetItem(DataUri.Parse(item.Fields["Datasource"].Value));
+
+                        if (testedItem != null)
+                        {
+                            testedItem.Editing.BeginEdit();
+                            testedItem.Appearance.ReadOnly = true;
+                            testedItem.Editing.EndEdit();
+                        }
+                        else
+                        {
+                            string errorMessage =
+                            string.Format(
+                                "Tested item is null. Please check if the \"Datasource\" field of the test definition item with ID = {0} is not empty.",
+                                item.ID.ToString());
+
+                            Log.Error(errorMessage, this);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
